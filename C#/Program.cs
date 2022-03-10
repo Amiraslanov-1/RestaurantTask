308a8d8a73bede0388b3c9667292b35ccd18a3a8
TasksAmiraslanov <tu7mxp4w4@code.edu.az>
2022-03-10 04:27:53 +0400

    Task added to Github

---
+++ C#/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Task_VS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+string month="Avqust";
+switch (month) 
+{
+  case "Yanvar" :
+    Console.WriteLine(31);
+    break;
+  case "Fevral":
+    Console.WriteLine(28);
+    break;
+  case "Mart":
+    Console.WriteLine(31);
+    break;
+  case "Aprel":
+    Console.WriteLine(30);
+    break;
+  case "May":
+    Console.WriteLine(31);
+    break;
+  case "Iyun":
+    Console.WriteLine(30);
+    break;
+  case "Iyul":
+    Console.WriteLine(31);
+    break;
+    case "Avqust":
+    Console.WriteLine(31);
+    break;
+    case "Sentyabr":
+    Console.WriteLine(30);
+    break;
+     case "Oktyabr":
+    Console.WriteLine(31);
+    break;
+    case "Noyabr":
+    Console.WriteLine(30);
+    break;
+    case "Dekabr":
+    Console.WriteLine(31);
+    break;
+}
+        }
+    }
+}
---
+++ a/Task-1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = {1,2,3,4};
+            foreach (var item in AddTo(out arr,6))
+            {
+                Console.WriteLine(item);
+            }
+           
+        }
+        static int[] AddTo( out int [] arr,int num)
+        {
+            
+            arr = new int[] { 1, 2, 3, 4,num };
+
+
+            return arr;
+        }
+    }
+}
