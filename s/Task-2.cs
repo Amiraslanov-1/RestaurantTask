ec8b97f95fe311cccf3d6bc7d245b378b3b95915
TasksAmiraslanov <tu7mxp4w4@code.edu.az>
2022-03-10 04:47:00 +0400

    Task 2 added

---
+++ s/Task-2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String Sentence = "Code Academy";
+            Array(ref Sentence);
+            Console.WriteLine(Sentence);
+        }
+        static void Array(ref string Sentence)
+        {
+            Sentence = "/CodeAcademy";
+        }
+    }
+}
